e822b8c87c3cf7470342716301cd648388122451
Pekka Siika-aho <pekka@Pekka-MacBook-Pro.local>
2015-02-08 19:21:22 +0200

    Changed to node intro app and removed nodemodules

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-	console.log("get request:" + req)
-  res.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('URL-shortener listening at http://%s:%s', host, port)
-
-})
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
