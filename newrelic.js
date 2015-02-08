d6de5bc14a870a74951e03cdb8d68d270249a4b1
Pekka Siika-aho <pekka@siika-aho.fi>
2015-02-08 23:24:35 +0200

    Added NewRelic tracking

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['url-shortener-reaktor'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'l07f22fff4ae5084e977cebf3f302433782892bfd',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
