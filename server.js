36754fcbf75782d53c8359c508f8716d47fdf705
Pekka Siika-aho <pekka@Pekka-MacBook-Pro.local>
2015-02-08 21:36:01 +0200

    Testing the shorten-post

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set("port", (process.env.PORT || 5000));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get("/", function(request, response) {
+  response.send("Hello World!");
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.get("/shorten", function(req, res) {
+	res.send("1234567");
+	console.log(req)
+})
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
 });
