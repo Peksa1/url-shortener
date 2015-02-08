ed1fdb1e9b603d0db830d6be2a63e8086f6b148c
Pekka Siika-aho <pekka@siika-aho.fi>
2015-02-08 21:50:01 +0200

    Added body-parser to populate req.body

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body_parser")
 
 app.set("port", (process.env.PORT || 5000));
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(request, response) {
   response.send("Hello World!");
